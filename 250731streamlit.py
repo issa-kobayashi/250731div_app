0eb52d59ca6eedc83deb29a9f3546f40cd161658
issa-kobayashi <issa580611@gmail.com>
2025-08-01 16:37:06 +0900

    250731-3

--- 250731streamlit.py
+++ 250731streamlit.py
@@ -77,7 +77,7 @@ with col2:
           image4=Image.open('./data250731re/IMG250514-13-01.jpg')  
           
           
-        if n=4 :
+        if n==4 :
           image1=Image.open('./data250731re/IMG250514-13-02.jpg')
           image2=Image.open('./data250731re/IMG250514-13-03.jpg') 
           image3=Image.open('./data250731re/IMG250514-14-04.jpg') 
