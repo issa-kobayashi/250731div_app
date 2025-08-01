8ed957b0200e19b7b55d9fdfa63cb881597e2624
issa-kobayashi <issa580611@gmail.com>
2025-08-01 17:33:57 +0900

    250731-5

--- 250731streamlit.py
+++ 250731streamlit.py
@@ -54,35 +54,36 @@ with col2:
           image2=Image.open('./data250731re/IMG241103-11-02.jpg') 
           image3=Image.open('./data250731re/IMG241103-11-03.jpg') 
           image4=Image.open('./data250731re/IMG241103-11-04.jpg') 
-          name=(
+          name='IMG241103-11-01','IMG241103-11-02','IMG241103-11-03','IMG241103-11-04'
           
         if n==1 :
           image1=Image.open('./data250731re/IMG241103-11-05.jpg')
           image2=Image.open('./data250731re/IMG241103-11-06.jpg') 
           image3=Image.open('./data250731re/IMG241103-12-07.jpg') 
           image4=Image.open('./data250731re/IMG241103-12-08.jpg') 
-          
+          name='IMG241103-11-05','IMG241103-11-06','IMG241103-11-07','IMG241103-11-08'
           
         if n==2 :
           image1=Image.open('./data250731re/IMG250212-09-01.jpg')
           image2=Image.open('./data250731re/IMG250212-09-02.jpg') 
           image3=Image.open('./data250731re/IMG250212-09-03.jpg') 
           image4=Image.open('./data250731re/IMG250302-14-01.jpg')     
-         
+          name='IMG250212-09-01','IMG250212-09-02','IMG250212-09-03','IMG250302-14-01'
          
         if n==3 :
           image1=Image.open('./data250731re/IMG250302-09-01.jpg')
           image2=Image.open('./data250731re/IMG250514-09-02.jpg') 
           image3=Image.open('./data250731re/IMG250514-09-03.jpg') 
           image4=Image.open('./data250731re/IMG250514-13-01.jpg')  
-          
+          name='IMG250302-09-01','IMG250514-09-02','IMG250514-09-03','IMG250514-13-01'
           
         if n==4 :
           image1=Image.open('./data250731re/IMG250514-13-02.jpg')
           image2=Image.open('./data250731re/IMG250514-13-03.jpg') 
           image3=Image.open('./data250731re/IMG250514-14-04.jpg') 
           image4=Image.open('./data250731re/IMG250514-14-05.jpg')   
-          
+          name='IMG250514-13-02','IMG250514-13-03','IMG250514-14-04','IMG250514-14-05'
+        
         st.image(image1,width=400)
         st.image(image2,width=400)
         st.image(image3,width=400)
