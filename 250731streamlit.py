24e3883a500090e47a465b151e896f633652c221
issa-kobayashi <issa580611@gmail.com>
2025-08-01 13:05:43 +0900

    250731-1

---
+++ 250731streamlit.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from PIL import Image
+# imageﾊpip install pillowｶﾞ必要 pil
+import time
+import datetime
+
+
+st.title('250731streamlit')
+st.caption('町内の為のホームページです｡')
+
+
+#streamlit run e:\250731div_app\250731streamlit.py
+#上記をｺﾏﾝﾄﾞﾌﾟﾛﾝﾌﾟﾄに打ち込みﾛｰｶﾙﾎｽﾄを起動させる
+
+
+#縦に分轄
+col1,col2=st.columns(2)
+
+with col1:
+        st.subheader('みずき野紹介')
+        
+        st.text('みずき野は田んぼに囲まれたとても静かな街です｡')
+        st.write('かがた公園')
+        
+        
+        #画像の登録
+        #image=Image.open('iris_tree.png')
+        image1=Image.open('./data250731re/IMG241103-11-01.jpg')
+        
+        #streamlitﾆimageｦ渡す
+        st.image(image1,width=400)
+        
+        image2=Image.open('./data250731re/IMG241103-11-02.jpg')
+        st.image(image2,width=400)
+        
+        image3=Image.open('./data250731re/IMG241103-11-03.jpg')
+        st.image(image3,width=400)
+            
+        image4=Image.open('./data250731re/IMG250514-13-01.jpg')
+        st.image(image4,width=400)
+        
+with col2:    
+        
+        submit_btn=st.form_submit_button('切替')
+        if submit_btn:
+            n=n+1
+            st.text(f'切替完 n=',n)
+            if 3<n :
+               n=0
+               
+         
+        if n=0 :
+          image1=Image.open('./data250731re/IMG241103-11-01.jpg')
+          image2=Image.open('./data250731re/IMG241103-11-02.jpg') 
+          image3=Image.open('./data250731re/IMG241103-11-03.jpg') 
+          image4=Image.open('./data250731re/IMG241103-11-04.jpg') 
+          
+          
+        if n=1 :
+          image1=Image.open('./data250731re/IMG241103-11-05.jpg')
+          image2=Image.open('./data250731re/IMG241103-11-06.jpg') 
+          image3=Image.open('./data250731re/IMG241103-12-07.jpg') 
+          image4=Image.open('./data250731re/IMG241103-12-08.jpg') 
+          
+          
+        if n=2 :
+          image1=Image.open('./data250731re/IMG250212-09-01.jpg')
+          image2=Image.open('./data250731re/IMG250212-09-02.jpg') 
+          image3=Image.open('./data250731re/IMG250212-09-03.jpg') 
+          image4=Image.open('./data250731re/IMG250302-14-01.jpg')     
+         
+         
+        if n=2 :
+          image1=Image.open('./data250731re/IMG250302-09-01.jpg')
+          image2=Image.open('./data250731re/IMG250514-09-02.jpg') 
+          image3=Image.open('./data250731re/IMG250514-09-03.jpg') 
+          image4=Image.open('./data250731re/IMG250514-13-01.jpg')  
+          
+          
+        if n=3 :
+          image1=Image.open('./data250731re/IMG250514-13-02.jpg')
+          image2=Image.open('./data250731re/IMG250514-13-03.jpg') 
+          image3=Image.open('./data250731re/IMG250514-14-04.jpg') 
+          image4=Image.open('./data250731re/IMG250514-14-05.jpg')   
+          
+        st.image(image1,width=400)
+        st.image(image2,width=400)
+        st.image(image3,width=400)
+        st.image(image4,width=400)
