633244fb423bb9a66fb088a4b0f7db7cbb116288
issa-kobayashi <issa580611@gmail.com>
2025-08-05 22:23:45 +0900

    250731-26

--- 250731streamlit.py
+++ 250731streamlit.py
@@ -107,7 +107,7 @@ with col2:
             if 4<n :
                n=0
                
-   process=step_one()
+   process=step-one()
    #step_one():  
         #mainp=main_process() #関数 def =することで実行される
         
